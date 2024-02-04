53ae4006006a920c57eba0a5005f0cf11ec80d82
Roxanne <reharding12@gmail.com>
2024-02-04 14:05:13 -0700

    passed 9 tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+console.log(mondayWork("work from home"));
+
+function wrapAdjective(wrapper) {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
